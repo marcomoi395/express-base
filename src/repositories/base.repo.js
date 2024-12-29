7e3a3344e508502f37cc33ea571d97d2464f33b2
Thanh Loi <marcomoi395@gmail.com>
2024-12-29 22:37:37 +0700

    feat(repo): upgrade base repositories

--- src/repositories/base.repo.js
+++ src/repositories/base.repo.js
@@ -3,12 +3,30 @@ class BaseRepository {
         this.model = model;
     }
 
-    async findAll() {
-        return this.model.find();
+    async find(
+        query = {},
+        options = { limit: "10", skip: 0, sort: {}, lean: true },
+    ) {
+        const { limit = "10", skip = 0, sort = {}, lean = true } = options;
+
+        return await this.model
+            .find(query)
+            .limit(Number(limit))
+            .skip(skip)
+            .sort(sort)
+            .lean(lean);
     }
 
-    async findById(id) {
-        return this.model.findById(id);
+    async findOne(query = {}, options = {}) {
+        const { lean = true } = options;
+
+        return this.model.find(query).lean(lean);
+    }
+
+    async findById(id, options = {}) {
+        const { lean = true } = options;
+
+        return this.model.findById(id).lean(lean);
     }
 
     async create(data) {
@@ -16,13 +34,28 @@ class BaseRepository {
         return newItem.save();
     }
 
-    async update(id, data) {
-        return this.model.findByIdAndUpdate(id, data, { new: true });
+    async findOneAndUpdate(query, data, options = { new: true }) {
+        return this.model.findOneAndUpdate(query, data, options);
     }
 
-    async delete(id) {
-        const result = await this.model.findByIdAndDelete(id);
-        return !!result;
+    async updateOne(query, data, options = {}) {
+        return this.model.updateOne(query, data, options);
+    }
+
+    async updateMany(query, data, options = {}) {
+        return this.model.updateMany(query, data, options);
+    }
+
+    async findOneAndDelete(query, options = {}) {
+        return this.model.findOneAndDelete(query, options);
+    }
+
+    async deleteOne(query, options = {}) {
+        return this.model.deleteOne(query, options);
+    }
+
+    async deleteMany(query, options = {}) {
+        return this.model.deleteMany(query, options);
     }
 }
 
